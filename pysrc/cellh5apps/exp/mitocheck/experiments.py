7aee7759b24645e17014987413928555293d6250
Christoph Sommer <christoph.sommer@imba.oeaw.ac.at>
2015-06-02 15:33:16 +0200

    just select a few positions for mito by row

--- pysrc/cellh5apps/exp/mitocheck/experiments.py
+++ pysrc/cellh5apps/exp/mitocheck/experiments.py
@@ -6,25 +6,9 @@ EXP = {'mito_check_test':
         'cellh5_files' : {
             'Screen_Plate_01': '/groups/gerlich/members/ChristophSommer/data/mito_check_ch5_repack/0001_02.ch5',
             },
-        'locations' : (
-            ("00001", 1),
-            ("00002", 1),
-            ("00003", 1),
-            ("00004", 1),
-            ("00005", 1),
-            ("00006", 1), 
-            ("00007", 1), 
-            ("00008", 1), 
-            ("00009", 1), 
-            ("00010", 1),
-            
-            ("00110", 1), 
-            ("00111", 1),
-            ("00112", 1),
-            ("00113", 1),
-            ),
-#         'rows' : list("A")[:],
-#         'cols' : tuple(range(4, 5)),
+         
+        'rows' : ["%05d" % d for d in range(1,30)],
+        'cols' : (1,),
         'training_sites' : (1,),
         }
        }   
